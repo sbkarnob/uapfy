fa50393e1aede49f0b86bdf5287c01dbfced76ce
tahiya <anisatahiya10@gmail.com>
2025-10-17 01:20:00 +0600

    URL

--- main/urls.py
+++ main/urls.py
@@ -1,14 +1,30 @@
 from django.contrib import admin
-from django.urls import path, include
-from django.conf import settings
+from django.urls import path
+from uapfy.views import *
 from django.conf.urls.static import static
+from django.conf import settings
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
+    path('', home, name='home'),
+    path('register/', register, name='register'),
+    path('login/', login, name='login'),
+    path('logout/', logout, name='logout'),
+
+
+    # organizer urls
+    path('register_organizer/', register_organizer, name='register_organizer'),
+    path('login_organizer/', login_organizer, name='login_organizer'),
+    path('logout_organizer/', logout_organizer, name='logout_organizer'),
+    path('organizer_dashboard/', dashboard, name='dashboard'),
+    
+
     path('admin/', admin.site.urls),
-    path('', include('uapfy.urls')),
+
+
 ]
 
-# Development mode এ media files serve করার জন্য
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+urlpatterns += staticfiles_urlpatterns()
