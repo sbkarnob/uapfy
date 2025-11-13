d199874224d2265688893a258e72470737ba398f
tahiya <anisatahiya10@gmail.com>
2025-11-13 17:35:23 +0600

    Upadate_URL

--- main/urls.py
+++ main/urls.py
@@ -16,7 +16,42 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from uapfy.views import *
+from django.conf.urls.static import static
+from django.conf import settings
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
+    path('', home, name='home'),
+    path('register/', register, name='register'),
+    path('login/', login, name='login'),
+    path('logout/', logout, name='logout'),
+    path('allevents/', event_view, name='event_view'),
+    path('events/<int:event_id>/', eventdetail, name='event_detail'),
+    path('event/<int:event_id>/buy/', buy_ticket, name='buy_ticket'),
+    path('my-tickets/', my_tickets, name='my_tickets'),
+    path('ticket/<int:ticket_id>/', ticket_detail, name='ticket_detail'),
+    path('user_profile/', user_profile, name='user_profile'),
+    path('contact/', ContactUs, name='ContactUs'),
+
+    # organizer urls
+    path('register_organizer/', register_organizer, name='register_organizer'),
+    path('login_organizer/', login_organizer, name='login_organizer'),
+    path('logout_organizer/', logout_organizer, name='logout_organizer'),
+    path('organizer_dashboard/', dashboard, name='dashboard'),
+    path('events/', event_list, name='event_list'),
+    path('events/create/', create_event, name='create_event'),
+    path('events/<int:event_id>/edit/', update_event, name='update_event'),
+    path('events/<int:event_id>/delete/', delete_event, name='delete_event'),
+    path('organizer/tickets/', organizer_tickets, name='organizer_tickets'),
+    path('organizer/analytics/', organizer_analytics, name='organizer_analytics'),
+    path('organizer/profile/', organizer_profile, name='organizer_profile'),
+
+    
     path('admin/', admin.site.urls),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+urlpatterns += staticfiles_urlpatterns()
