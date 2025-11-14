899d2af7a9dff412a09d440fd1f5cb0d7312c313
tahiya <anisatahiya10@gmail.com>
2025-11-14 12:13:52 +0600

    Update_UserProfile

--- uapfy/models.py
+++ uapfy/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
+import qrcode
+from io import BytesIO
+from django.core.files import File
+import uuid
+from decimal import Decimal
 
-# Create your models here.
+
+# User Profile
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    contact_number = models.CharField(max_length=15, blank=True, null=True)
+    address = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return self.user.username
