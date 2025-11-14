531ea3d5e97a72e2898898b0abf28d870052b85a
tahiya <anisatahiya10@gmail.com>
2025-11-14 12:16:48 +0600

    Organizer_Pofile

--- uapfy/models.py
+++ uapfy/models.py
@@ -16,3 +16,16 @@ class UserProfile(models.Model):
 
     def __str__(self):
         return self.user.username
+    
+
+    # Organizer Profile
+class OrganizerProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    organization_name = models.CharField(max_length=255, blank=True, null=True)
+    organization_address = models.TextField(blank=True, null=True)
+    contact_number = models.CharField(max_length=15, blank=True, null=True)
+    logo = models.ImageField(upload_to='organizer_logos/', blank=True, null=True)
+    description = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return self.organization_name or self.user.username
