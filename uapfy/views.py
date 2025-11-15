7aa6aa6e6eb348d787fc3026cdd16e1d1e770f1d
tahiya <anisatahiya10@gmail.com>
2025-11-15 12:02:50 +0600

    Views_HomePage

--- uapfy/views.py
+++ uapfy/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.contrib.auth import login as auth_login, logout as auth_logout, authenticate
+from django.contrib.auth.models import User
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib import messages
+from .models import *
+from django.contrib.auth.decorators import login_required
+from django.utils import timezone
+from django.db.models import Q 
+
 
 # Create your views here.
+def home(request):
+    # events = Event.objects.filter(is_active=True).order_by('-start_time')[:5] 
+    return render(request, 'home.html')
